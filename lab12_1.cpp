178da5fa30027073a89a2a89e25723c2cdcb1aef
TunyakarnK <tunyakarn2544@gmail.com>
2021-01-19 17:42:59 +0700

    lab12

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,34 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat (const double x[],int y,double z[]){
+    double sum=0;
+    for(int i=0;i<y;i++){
+        sum+=x[i];
+    }
+    double sd=0;
+    for(int i=0;i<y;i++){
+        sd+=pow((x[i]-(sum/y)),2);
+    }
+   double gm;
+	for (int i = 0; i < y; i++) 
+        gm = gm + log(x[i]); 
+    gm = gm / y;
+    double hm=0;
+    for(int i=0;i<y;i++){
+        hm+=1/x[i];
+	}
+	for(int i=0;i<y;i++){
+        if (x[i] > z[4]) 
+        z[4] = x[i];
+	}
+	z[5]=x[0];
+	for(int i=0;i<y;i++){
+        if (z[5]>x[i]  ) 
+        z[5] = x[i];
+	}
+    z[0]=sum/y;
+    z[1]=pow(sd/y,0.5);
+    z[2]=exp(gm);
+    z[3]=y/hm;
+}
